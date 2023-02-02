1fd7ae2493e9dba42211b08a4ee65bcab2dc311e
ROL - Rendimento Online <filipe_alvesoliveira@hotmail.com>
2023-02-02 19:52:00 -0300

    remove commenty

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -65,4 +65,3 @@ export default App;
 //props - atributos passados dentro de um component <Header title=filipe /> Informacoes q um component pai passa para um component filho
 //estado - uma informacao q o component vai armazenar e manipular essa info, armazena uma informacao dentro de uma var q eu mesmo defino o nome e uma function q seta a variavel, guardando a info anterior e cria uma nova .
 
-//31:35 aula4
